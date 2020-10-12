42b8c4e6c37157671aa135fbc316f81de04c5c3d
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-10-12 23:45:05 +0300

    Show preview data when closing a section

--- src/components/OrderPage/OrderPage.js
+++ src/components/OrderPage/OrderPage.js
@@ -1,7 +1,7 @@
 import CheckingCartSection from '../CheckingCartSection';
 import CustomerDataSection from '../CustomerDataSection';
 import ShippingSection from '../ShippingSection';
-import PaymentScreen from '../PaymentSection';
+import PaymentSection from '../PaymentSection';
 
 import './order-page.css';
 
@@ -10,9 +10,22 @@ const OrderPage = {
     const infoSectionContainers = document.querySelectorAll('.info-section-container');
     let activeInfoSection = document.querySelector('.active-info-section');
 
+    const mappingSection = {
+      checkingCart: CheckingCartSection,
+      customerData: CustomerDataSection,
+      shipping: ShippingSection,
+      payment: PaymentSection,
+    }
+
     infoSectionContainers.forEach((section) => { 
       section.addEventListener('click', ({ target }) => { 
         if (section !== activeInfoSection) {
+          const step = activeInfoSection.getAttribute('data-step');
+          const preview = activeInfoSection.querySelector('.info-section-preview');
+          if (preview) {
+            const previewContent = mappingSection[step].generatePreviewData();
+            preview.innerHTML = previewContent;
+          }
           activeInfoSection.classList.remove('active-info-section');
 
           section.classList.add('active-info-section');
@@ -23,14 +36,14 @@ const OrderPage = {
 
     CustomerDataSection.afterRender();
     ShippingSection.afterRender();
-    PaymentScreen.afterRender();
+    PaymentSection.afterRender();
   },
 
   render: () => {
     let content = CheckingCartSection.render();
     content += CustomerDataSection.render();
     content += ShippingSection.render();
-    content += PaymentScreen.render();
+    content += PaymentSection.render();
     const container = document.getElementById('main-container');
     container.innerHTML = content;
   },
